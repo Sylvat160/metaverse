0a01ff80122d3279020b0ae610b9bb801460e2da
Sylvain T <sylvaintagnabou@gmail.com>
2023-04-23 18:58:34 +0000

    (add) : gradient parts

--- app/page.js
+++ app/page.js
@@ -1,19 +1,37 @@
-import { Footer, Navbar } from '../components';
-import { About, Explore, Feedback, GetStarted, Hero, Insights, WhatsNew, World } from '../sections';
+import { Footer, Navbar } from '../components'
+import {
+  About,
+  Explore,
+  Feedback,
+  GetStarted,
+  Hero,
+  Insights,
+  WhatsNew,
+  World,
+} from '../sections'
 
 const Page = () => (
-  <div className='bg-primary-black overflow-hidden'>
+  <div className="bg-primary-black overflow-hidden">
     <Navbar />
     <Hero />
     <About />
-    <Explore />
-    <GetStarted />
-    <WhatsNew />
-    <World />
-    <Insights />
-    <Feedback />
+    <div className="relative">
+      <Explore />
+      <div className="gradient-03 z-0" />
+      <GetStarted />
+    </div>
+    <div className="relative">
+      <WhatsNew />
+      <div className="gradient-04 z-0" />
+      <World />
+    </div>
+    <div className="relative">
+      <Insights />
+      <div className="gradient-04 z-0" />
+      <Feedback />
+    </div>
     <Footer />
   </div>
-);
+)
 
-export default Page;
+export default Page
